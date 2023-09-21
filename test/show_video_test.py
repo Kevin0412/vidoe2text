bc13e914a8cb169648c3a806201659325912d595
Kevin0412 <45508696+Kevin0412@users.noreply.github.com>
2023-09-21 22:24:02 +0800

    3

--- test/show_video_test.py
+++ test/show_video_test.py
@@ -12,7 +12,7 @@ cap = cv2.VideoCapture("test/只因你太美(鸡你太美)原版.只因你太美
 while cap.isOpened():
     ret, frame = cap.read()
     if ret:
-        out=video2text.transfer(frame,25,120)
+        out=video2text.transfer(frame,64,128)
         print(out)
         with open("test/jntm/({}).txt".format(n),"w") as f:
             f.write(out)
