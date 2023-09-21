2c8167fd57193eb43200587dd7242aa44cdb26f7
Kevin0412 <45508696+Kevin0412@users.noreply.github.com>
2023-09-21 23:11:17 +0800

    4

--- test/show_video_test.py
+++ test/show_video_test.py
@@ -9,14 +9,17 @@ import numpy as np
 
 n=1
 cap = cv2.VideoCapture("test/只因你太美(鸡你太美)原版.只因你太美(鸡你太美)原版.90717632.mp4")
+file=open("test/jntm.txt","w")
 while cap.isOpened():
     ret, frame = cap.read()
     if ret:
-        out=video2text.transfer(frame,64,128)
+        out=video2text.transfer(frame,32,127)
         print(out)
         with open("test/jntm/({}).txt".format(n),"w") as f:
             f.write(out)
+        file.write(out)
         n+=1
     else:
         break
 cap.release()
+file.close()
